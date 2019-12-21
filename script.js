c858dcfb0bddde4f65b20400c6428f64c609e512
Zzyzx <rustyshackleford3@protonmail.com>
2019-12-21 15:45:22 -0800

    implemented event listeners and populate display function

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ function operate(operator, num1, num2) {
         return 'unrecognized operator';
     }
 }
+
+function populateDisplay(num) {
+    if (num == 'c') {
+        document.getElementById('display').innerText = '';
+    } else if (num == '=') {
+        
+    } else {
+        document.getElementById('display').innerText += num;
+    }
+}
+
+let numbers = document.getElementsByClassName('number');
+let operators = document.getElementsByClassName('operator');
+for (i=0; i<numbers.length; i++) {
+    numbers[i].addEventListener('click', function (event) {
+        let num = this.id;
+        populateDisplay(num);
+      }); 
+}
+for (i=0; i<operators.length; i++) {
+    operators[i].addEventListener('click', function (event) {
+        let oper = this.id;
+        populateDisplay(oper);
+      }); 
+}
